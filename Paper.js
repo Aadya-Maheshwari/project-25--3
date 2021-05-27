148a3c04a24e5ec03747f35b338db256f47463a9
Aadya-Maheshwari <76033806+Aadya-Maheshwari@users.noreply.github.com>
2021-05-27 12:27:47 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -18,7 +18,8 @@ class Paper{
       display(){
          push()
          translate(this.body.position.x,this.body.position.y);
-         image(this.image);
+         imageMode(CENTER);
+         image(this.image,0,0,this.r,this.r);
          pop()
       }
 }
